8d1aceca04bdcbcdb67e1021721b4c183263c810
dalismiles <dali.iapichino@gmail.com>
2022-09-23 18:48:31 +0200

    fix:services path changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Hero from './components/Hero'
 
-import {app,db, querySnapshot} from './services/firebase'
+import {app,db, querySnapshot} from '../services/firebase'
 
 export default function Home() {
 
